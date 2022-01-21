d53b98b08dfe7ab01e13bb824fa11909f05d7b7d
mmaia <maia.marcos@gmail.com>
2022-01-21 11:24:50 +0100

    Initial commit with generated project

---
+++ src/test/kotlin/com/maia/simplespringkafkastreamkotlin/SimpleSpringKafkaStreamKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.maia.simplespringkafkastreamkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleSpringKafkaStreamKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
